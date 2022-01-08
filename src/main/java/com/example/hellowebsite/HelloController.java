4f8d12d40b4b0e604a69c37e1a227611c47b1f84
lje-ejl <zizz1357@hanmail.net>
2022-01-08 21:45:47 +0900

    indexController -> HelloController

--- src/main/java/com/example/hellowebsite/HelloController.java
+++ src/main/java/com/example/hellowebsite/HelloController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class indexController {
+public class HelloController {
 
     @GetMapping("/hello")
     public String index(){
-        return "Hello!!!";
+        return "Hello";
     }
 
 }
